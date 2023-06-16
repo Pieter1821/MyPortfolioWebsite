0f8fd78ffdbc94a4835fee8fc4e473b53f5a8c58
Pieter Deane <deanepieter@gmail.com>
2023-06-16 16:29:22 +0200

    added a lazy loading function and a spinner to make it more user friendly

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { Suspense,useEffect,useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/home';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Skills from './pages/Skills';
-import Contact  from './pages/contact';
+import Spinner from './components/Spinner';
 
+// Lazy-loaded components
 
-
-
+const Home = React.lazy(() => import('./pages/home'));
+const About = React.lazy(() => import('./pages/about'));
+const Projects = React.lazy(() => import('./pages/projects'));
+const Skills = React.lazy(() => import('./pages/skills'));
+const Contact = React.lazy(() => import('./pages/contact'));
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay of 2 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+   
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+    
+  }
+
   return (
     <div className="App">
       <Router>
         <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path='/Contact' element={<Contact/>} />'
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
 
         <Footer />
       </Router>
     </div>
   );
 }
-
